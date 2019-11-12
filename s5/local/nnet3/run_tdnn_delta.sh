88924a8bee7e21eb62954ae614f586aa9c0e52fc
mostafashahin <mostafa_shahin@ieee.org>
2019-11-12 16:08:07 +1100

    Fixed a bug in xconfig format

--- s5/local/nnet3/run_tdnn_delta.sh
+++ s5/local/nnet3/run_tdnn_delta.sh
@@ -9,7 +9,7 @@
 # note: the last column is a version of tdnn_d that was done after the
 # changes for the 5.1 version of Kaldi (variable minibatch-sizes, etc.)
 
-stage=9
+stage=10
 affix=
 train_stage=-10
 has_fisher=false
@@ -53,11 +53,12 @@ if [ $stage -le 9 ]; then
   cat <<EOF > $dir/configs/network.xconfig
   input dim=100 name=ivector
   input dim=40 name=input
-  delta-layer name=delta input=Append(-2,-1,0,1,2)
+  # delta-layer name=delta input=Append(-2,-1,0,1,2)
+  delta-layer name=delta 
   # please note that it is important to have input layer with the name=input
   # as the layer immediately preceding the fixed-affine-layer to enable
   # the use of short notation for the descriptor
-  no-op-component name=input2 input=Append(delta,ReplaceIndex(ivector, t, 0))
+  no-op-component name=input2 input=Append(-2,-1,0,1,2,ReplaceIndex(ivector, t, 0))
 
   # the first splicing is moved before the lda layer, so no splicing here
   relu-renorm-layer name=tdnn1 input=input2 dim=1024
@@ -77,7 +78,7 @@ if [ $stage -le 10 ]; then
 
   steps/nnet3/train_dnn.py --stage=$train_stage \
     --cmd="$decode_cmd" \
-    --feat.online-ivector-dir exp/nnet3_vp/ivectors_${train_set} \
+    --feat.online-ivector-dir exp/nnet3_vp_delta/ivectors_${train_set} \
     --feat.cmvn-opts="--norm-means=false --norm-vars=false" \
     --trainer.num-epochs 2 \
     --trainer.optimization.num-jobs-initial 1 \
@@ -103,7 +104,7 @@ if [ $stage -le 11 ]; then
     (
     num_jobs=`cat data/${decode_set}_hires/utt2spk|cut -d' ' -f2|sort -u|wc -l`
     steps/nnet3/decode.sh --nj $num_jobs --cmd "$decode_cmd" \
-      --online-ivector-dir exp/nnet3_vp/ivectors_${decode_set} \
+      --online-ivector-dir exp/nnet3_vp_delta/ivectors_${decode_set} \
       $graph_dir data/${decode_set}_hires $dir/decode_${decode_set}_hires || exit 1;
     ) &
   done
